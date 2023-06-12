13e5af72b92bb2fe10edc9472d114bfa2a82fe2f
Permy Ghataorre <pghataorre@and.digital>
2023-06-12 10:22:24 +0100

    Added in Stretch goal - to show videos

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
-    window.onload = () => {
-      attachSliderEvents();
-    } 
+window.onload = () => {
+  attachSliderEvents();
+  attachShowHideVideoEvents();
+} 
 
-    const attachSliderEvents = () => {
-      const rangeSliders = document.querySelectorAll('input[type=range]');
+const attachSliderEvents = () => {
+  const rangeSliders = document.querySelectorAll('input[type=range]');
+  if (rangeSliders === undefined || rangeSliders.length == 0) return;
 
-      rangeSliders.forEach((rangeItem) => {
-        rangeItem.addEventListener("change", (event) => {
-          let sliderValue = event.target.value;
-          let outputElementToChange = event.currentTarget.nextElementSibling
-          moveSlider(sliderValue, outputElementToChange);
-        });
-      });
-    }
+  rangeSliders.forEach((rangeItem) => {
+    rangeItem.addEventListener('change', (event) => {
+      let sliderValue = event.target.value;
+      let outputElementToChange = event.currentTarget.nextElementSibling
+      moveSlider(sliderValue, outputElementToChange);
+    });
+  });
+}
 
-    const moveSlider = (sliderValue, nextSibling) => {
-      nextSibling.innerText = sliderValue;
-    }
+const moveSlider = (sliderValue, nextSibling) => {
+  nextSibling.innerText = sliderValue;
+}
+
+const attachShowHideVideoEvents = () => {
+  const toggleButtons = document.querySelectorAll('.video-display-toggle');
+  if (toggleButtons === undefined || toggleButtons.length == 0) return;
+
+
+  toggleButtons.forEach((buttonItem, index) => {
+    buttonItem.addEventListener('click', (event) => {
+      let videoBlock = document.getElementsByClassName('video')[index];
+      videoBlock.classList.add('video--show');
+      
+    });
+  });
+}
+
+
